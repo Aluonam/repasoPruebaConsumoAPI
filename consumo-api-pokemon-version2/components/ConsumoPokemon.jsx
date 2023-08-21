eec4578d0eed84036762417d4e53444dc44e0cda
Aluon <paulacanoroman@gmail.com>
2023-08-21 21:54:03 +0200

    función llamadaAPI

--- consumo-api-pokemon-version2/components/ConsumoPokemon.jsx
+++ consumo-api-pokemon-version2/components/ConsumoPokemon.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 const ConsumoPokemon = () => {
 
 
+    const llamadaAPI = async () => {
+        const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+        const llamada = await fetch(url);
+        const datos = await llamada.json();
+        console.log(datos);
+    }
+
+    llamadaAPI()
+
   return (
     <div>ConsumoPokemon</div>
   )
