cfd64a3f4325b78fda5a1bc128c0f8b34dd76f05
Aluon <paulacanoroman@gmail.com>
2023-08-21 22:01:13 +0200

    useState no funcional

--- consumo-api-pokemon-version2/components/ConsumoPokemon.jsx
+++ consumo-api-pokemon-version2/components/ConsumoPokemon.jsx
@@ -2,19 +2,22 @@ import React, { useState } from 'react'
 
 const ConsumoPokemon = () => {
 
+    const [datosAPI, setDatosAPI] = useState('')
+    const [datosInput, setDatosInput] = useState('')
 
     const llamadaAPI = async () => {
         const url = "https://pokeapi.co/api/v2/pokemon/ditto";
         const llamada = await fetch(url);
         const datos = await llamada.json();
-        console.log(datos);
+        setDatosAPI(datos);
     }
 
     
 
   return (
     <>
-    <div>Consumo API Pokemon</div>
+    <div>Introduce el nombre de un pokemon</div>
+    <input onChange={()=>{}}></input>
     <button onClick={()=>{llamadaAPI()}}>Clic</button>
     </>
   )
