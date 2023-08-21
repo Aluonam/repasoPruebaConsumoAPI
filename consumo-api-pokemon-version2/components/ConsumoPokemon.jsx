ce455a7a31f6350046d31a952c2c951072bb2ef2
Aluon <paulacanoroman@gmail.com>
2023-08-21 22:21:10 +0200

    funcionalidad sin nombre especifico

--- consumo-api-pokemon-version2/components/ConsumoPokemon.jsx
+++ consumo-api-pokemon-version2/components/ConsumoPokemon.jsx
@@ -6,18 +6,23 @@ const ConsumoPokemon = () => {
     const [datosInput, setDatosInput] = useState('')
 
     const llamadaAPI = async () => {
-        const url = "https://pokeapi.co/api/v2/pokemon/ditto";
-        const llamada = await fetch(url);
-        const datos = await llamada.json();
-        setDatosAPI(datos);
+        try{
+            const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+            const llamada = await fetch(url);
+            const datos = await llamada.json();
+            setDatosAPI(datos.sprites.back_default);
+        } catch(error){
+        console.log("Error detectado",error)
+        }
     }
 
     
 
   return (
     <>
+    {datosAPI !== '' && <img src={datosAPI}/>}
     <div>Introduce el nombre de un pokemon</div>
-    <input onChange={()=>{}}></input>
+    <input onChange={(e)=>{setDatosInput(e.target.value)}}></input>
     <button onClick={()=>{llamadaAPI()}}>Clic</button>
     </>
   )
