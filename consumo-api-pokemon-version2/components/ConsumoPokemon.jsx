7113b8428da31a2c015be4ee34961d4e49b21eee
Aluon <paulacanoroman@gmail.com>
2023-08-21 23:58:27 +0200

    funcionalidad con nombre y con 2 imagenes

--- consumo-api-pokemon-version2/components/ConsumoPokemon.jsx
+++ consumo-api-pokemon-version2/components/ConsumoPokemon.jsx
@@ -5,25 +5,33 @@ const ConsumoPokemon = () => {
     const [datosAPI, setDatosAPI] = useState('')
     const [datosInput, setDatosInput] = useState('')
 
-    const llamadaAPI = async () => {
+    const llamadaAPI = async (nombrePokemon) => {
         try{
-            const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+            const url = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`;
             const llamada = await fetch(url);
             const datos = await llamada.json();
-            setDatosAPI(datos.sprites.back_default);
+            // setDatosAPI(datos.sprites._default);
+            setDatosAPI({
+              cara: datos.sprites.front_default,
+              culo: datos.sprites.back_default
+            });
         } catch(error){
         console.log("Error detectado",error)
         }
     }
+    const imagenes = <>
+                    <img src={datosAPI.cara}/>
+                    <img src={datosAPI.culo}/>
+                    </>
 
     
 
   return (
     <>
-    {datosAPI !== '' && <img src={datosAPI}/>}
+    {datosAPI !== '' && imagenes}
     <div>Introduce el nombre de un pokemon</div>
     <input onChange={(e)=>{setDatosInput(e.target.value)}}></input>
-    <button onClick={()=>{llamadaAPI()}}>Clic</button>
+    <button onClick={()=>{llamadaAPI(datosInput)}}>Clic</button>
     </>
   )
 }
